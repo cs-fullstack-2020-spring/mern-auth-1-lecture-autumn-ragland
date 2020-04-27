45fcd1890b7fd6ee7fcb24d6bac61b4a882d7b3d
autumn-ragland <raglanda998@gmail.com>
2020-04-27 15:44:52 -0500

    login user from client and add comments

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,47 +2,66 @@ import React, { Component, Fragment } from "react";
 // import {BrowserRouter as Route, Router, Link } from "react-router-dom";
 
 // class based component to login an existing user from the database via form submission
-class Login extends Component{
-    constructor(props){
+class Login extends Component {
+    constructor(props) {
         super(props);
         // set props of state for controlled component form
         this.state = {
-            password : "",
-            email : "",
+            password: "",
+            email: "",
         }
     }
 
     // controlled component form
     handleChange = (event) => {
-        if(event.target.name === "email"){
-            this.setState({email : event.target.value});
-        } else if(event.target.name === "password"){
-            this.setState({password : event.target.value});
+        if (event.target.name === "email") {
+            this.setState({ email: event.target.value });
+        } else if (event.target.name === "password") {
+            this.setState({ password: event.target.value });
         }
     }
 
     // when form is submitted read user from database
-    handleSubmission = (event) => {
-        event.preventDefault();
-        console.log(this.state);
+    handleSubmission = async (event) => {
+        event.preventDefault(); // keep page from reloading
+        // console.log(this.state);
+
+        // define object to send to post request
+        let user = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        // fetch server endpoint 
+        let response = await fetch('/users/login', {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+        // pull out json from response
+        let json = await response.json();
+        // log json response from server
+        console.log(json);
     }
-    
+
     // render form
-    render(){
-        return(
+    render() {
+        return (
             <Fragment>
                 <h1>Login</h1>
                 <form>
 
                     <label htmlFor="email">Email</label>
-                    <input type="text" name="email" id="email" value = {this.state.email} onChange = {this.handleChange}/>
-                    <br/>
+                    <input type="text" name="email" id="email" value={this.state.email} onChange={this.handleChange} />
+                    <br />
 
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" value = {this.state.password} onChange = {this.handleChange}/>
-                    <br/>
+                    <input type="password" name="password" id="password" value={this.state.password} onChange={this.handleChange} />
+                    <br />
 
-                    <button onClick = {this.handleSubmission}>Login</button>
+                    <button onClick={this.handleSubmission}>Login</button>
                 </form>
             </Fragment>
         )
--- client/src/components/Registration.jsx
+++ client/src/components/Registration.jsx
@@ -2,25 +2,25 @@ import React, { Component, Fragment } from "react";
 // import {BrowserRouter as Route, Router, Link } from "react-router-dom";
 
 // class based component to create a new user in the database via form submission
-class Registration extends Component{
-    constructor(props){
+class Registration extends Component {
+    constructor(props) {
         super(props);
         // set props of state for controlled component form
         this.state = {
-            name : "",
-            password : "",
-            email : "",
+            name: "",
+            password: "",
+            email: "",
         }
     }
 
     // controlled component form
     handleChange = (event) => {
-        if(event.target.name === "name"){
-            this.setState({name : event.target.value});
-        } else if(event.target.name === "email"){
-            this.setState({email : event.target.value});
-        } else if(event.target.name === "password"){
-            this.setState({password : event.target.value});
+        if (event.target.name === "name") {
+            this.setState({ name: event.target.value });
+        } else if (event.target.name === "email") {
+            this.setState({ email: event.target.value });
+        } else if (event.target.name === "password") {
+            this.setState({ password: event.target.value });
         }
     }
 
@@ -31,18 +31,18 @@ class Registration extends Component{
 
         // define object to send to post request
         let newUser = {
-            name : this.state.name,
-            email : this.state.email,
-            password : this.state.password
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password
         }
         // fetch server endpoint 
         let response = await fetch('/users/register', {
-            method : "POST",
+            method: "POST",
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body : JSON.stringify(newUser)
+            body: JSON.stringify(newUser)
         });
         // pull out json from response
         let json = await response.json();
@@ -50,24 +50,24 @@ class Registration extends Component{
         console.log(json);
     }
     // render form
-    render(){
-        return(
+    render() {
+        return (
             <Fragment>
                 <h1>Registration</h1>
                 <form>
                     <label htmlFor="name">Name</label>
-                    <input type="text" name="name" id="name" value = {this.state.name} onChange = {this.handleChange}/>
-                    <br/>
+                    <input type="text" name="name" id="name" value={this.state.name} onChange={this.handleChange} />
+                    <br />
 
                     <label htmlFor="email">Email</label>
-                    <input type="text" name="email" id="email" value = {this.state.email} onChange = {this.handleChange}/>
-                    <br/>
+                    <input type="text" name="email" id="email" value={this.state.email} onChange={this.handleChange} />
+                    <br />
 
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" value = {this.state.password} onChange = {this.handleChange}/>
-                    <br/>
+                    <input type="password" name="password" id="password" value={this.state.password} onChange={this.handleChange} />
+                    <br />
 
-                    <button onClick = {this.handleSubmission}>Register</button>
+                    <button onClick={this.handleSubmission}>Register</button>
                 </form>
             </Fragment>
         )
